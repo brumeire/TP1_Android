094c3233e2ba066a5d0d16275b59ad3adff37226
GrosOurs88 <hugo.bourbon@wanadoo.fr>
2017-10-27 13:04:19 +0200

    Add Victory State and Script (ActivateArcheScript)

---
+++ Assets/Scripts/ActivateArcheScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActivateArcheScript : MonoBehaviour 
+{
+	private GameObject[] pieceOfArche;
+
+
+	void OnTriggerStay(Collider other)
+	{			
+		if (other.gameObject.tag == "Trigger") 
+		{
+			RaycastHit hit;
+			Debug.DrawRay (transform.position, transform.forward, Color.red, 1f);
+
+			if (Physics.Raycast (transform.position, transform.forward, out hit))
+			{
+				if (hit.transform.tag == "Arche") 
+				{
+					pieceOfArche = GameObject.FindGameObjectsWithTag ("Arche");
+					foreach (GameObject piece in pieceOfArche) 
+					{
+						piece.GetComponent<Renderer> ().material.color = Color.blue;
+					}
+				}
+			}
+		}
+	}
+}
