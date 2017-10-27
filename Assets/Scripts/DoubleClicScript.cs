aa7e986566dc2faff963b287af7362b6284d422d
GrosOurs88 <hugo.bourbon@wanadoo.fr>
2017-10-27 11:05:09 +0200

    Script Double Clic

--- Assets/Scripts/DoubleClicScript.cs
+++ Assets/Scripts/DoubleClicScript.cs
@@ -2,15 +2,45 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DoubleClicScript : MonoBehaviour {
+public class DoubleClicScript : MonoBehaviour
+{
+	int idx;
+	public float delai;
 
-	// Use this for initialization
-	void Start () {
-		
+	void Start () 
+	{
+
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+
+	void Update () 
+	{
+		for (int i = 0; i < Input.touchCount; ++i)
+		{
+			if (Input.GetTouch(i).phase == TouchPhase.Began) 
+			{
+				if (Input.GetTouch(i).tapCount == 2)
+				{
+					StartCoroutine (MoveCameraForward());
+				}
+			}
+			print (Input.GetTouch(i).tapCount);
+		}
+			
+
+		/*
+		if (Input.GetKeyDown (KeyCode.Space)) 
+		{
+			StartCoroutine (MoveCameraForward());
+		}
+		*/
+	}
+
+	public IEnumerator MoveCameraForward()
+	{
+		for (int i = 0; i < delai; i++) 
+		{
+			transform.Translate (Vector3.forward);
+			yield return new WaitForFixedUpdate();
+		}
 	}
 }
