414346703dea530ad8fb9efd67ef44213c3eedd3
GrosOurs88 <hugo.bourbon@wanadoo.fr>
2017-10-27 10:06:14 +0200

    Scene_Cube_Rotate

---
+++ Assets/Scripts/RotateCube.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateCube : MonoBehaviour 
+{
+
+	void Start () 
+	{
+		
+	}
+	
+
+	void Update () 
+	{
+		transform.Rotate (100f * Time.deltaTime, 80f * Time.deltaTime, 75f * Time.deltaTime);
+	}
+}
