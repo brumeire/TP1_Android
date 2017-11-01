7c801ffbc726b38952694e5556346496c4fc81c9
GrosOurs88 <hugo.bourbon@wanadoo.fr>
2017-11-01 13:51:21 +0100

    Creation et activation Arche Ok

--- Assets/Scripts/ActivateArcheScript.cs
+++ Assets/Scripts/ActivateArcheScript.cs
@@ -5,8 +5,19 @@ using UnityEngine;
 public class ActivateArcheScript : MonoBehaviour 
 {
 	private GameObject[] pieceOfArche;
+	public GameObject[] pieceOfArcheFusionnee;
+	private GameObject archeFusionnee;
+	private GameObject portail;
 
-
+	void Start()
+	{
+		archeFusionnee = GameObject.Find ("Arche_Fusionnee");
+		portail = GameObject.Find ("Portail");
+		archeFusionnee.SetActive (false);
+		portail.GetComponent<Transform> ().localScale = Vector3.zero;
+		portail.SetActive (false);
+	}
+		
 	void OnTriggerStay(Collider other)
 	{			
 		if (other.gameObject.tag == "Trigger") 
@@ -21,10 +32,28 @@ public class ActivateArcheScript : MonoBehaviour
 					pieceOfArche = GameObject.FindGameObjectsWithTag ("Arche");
 					foreach (GameObject piece in pieceOfArche) 
 					{
-						piece.GetComponent<Renderer> ().material.color = Color.blue;
+						piece.SetActive (false);
 					}
+					archeFusionnee.SetActive (true);
+					foreach (GameObject go in pieceOfArcheFusionnee) 
+					{
+						go.SetActive (true);
+					}
+					new WaitForSeconds (1f);
+					portail.SetActive (true);
+
+					StartCoroutine ("LaunchPortal");
 				}
 			}
+		}
+	}
+
+	public IEnumerator LaunchPortal()
+	{
+		while (portail.GetComponent<Transform> ().localScale.x < 38f) 
+		{
+			portail.GetComponent<Transform> ().localScale += new Vector3(3.8f,1f,3.2f) * Time.deltaTime;
+			yield return new WaitForFixedUpdate();
 		}
 	}
 }
