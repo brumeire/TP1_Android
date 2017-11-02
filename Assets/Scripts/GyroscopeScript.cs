f6e0ca1fe84fd9f073e4f21e59e9d7d1032a62dd
lmoge <ludovic.moge@gmail.com>
2017-11-02 21:02:49 +0100

    gyro update

--- Assets/Scripts/GyroscopeScript.cs
+++ Assets/Scripts/GyroscopeScript.cs
@@ -6,18 +6,46 @@ using UnityEngine.UI;
 [RequireComponent(typeof(Camera))]
 public class GyroscopeScript : MonoBehaviour 
 {
-	public float speed = 0.2f;
+
+	[SerializeField]
+	private float rotationSpeed = 0.2f;
 	public Text text;
+
+	void Start() 
+	{
+		Input.gyro.enabled = true;
+	}
 
 	void Update()
 	{
-		Vector3 input = Input.acceleration;
-		transform.Rotate(input*speed*Time.deltaTime);
-		text.text = Input.acceleration.ToString();
+		transform.rotation = ConvertRot(Input.gyro.attitude);
+		text.text = Input.gyro.attitude.ToString();
 	}
 
-
+	private Quaternion ConvertRot(Quaternion input)
+	{
+		return new Quaternion(-input.y, input.x, input.z, -input.w);
+	}	
 	#region old
+
+// public float speed = 0.2f;
+	// public Text text;
+
+	// void Update()
+	// {
+	// 	Vector3 input = Input.acceleration;
+	// 	transform.Rotate(input*speed*Time.deltaTime);
+	// 	text.text = Input.acceleration.ToString();
+		
+	// }
+/*
+	X = gauche-droite : gauche -1 - droite 1
+	
+	Z = face vers le haut : haut -1 - bas -1
+
+ */
+
+
 	/*
 	[SerializeField]
 	private float rotationSpeed =0.2f;
